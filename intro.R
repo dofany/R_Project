6dcfc0760e027b991bbd628e6427cafebda013d6
dofany <kimdohwan17@gmail.com>
2021-04-30 17:28:55 +0900

    버전 업그레이드 설명 및 패키지 업데이트

---
+++ intro.R
@@ -0,0 +1,18 @@
+#### 버전 업그레이드 ####
+install.packages("installr")
+library(installr)
+updateR()
+
+# 패키지 업데이트
+update.packages()
+
+# Python Anaconda에서 R 실행
+# conda install -c r r-essentials
+
+# 도움말
+help(print)
+?sum
+help("for")
+example(lm)
+
+
